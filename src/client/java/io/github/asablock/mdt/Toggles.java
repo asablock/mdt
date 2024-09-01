8e0c002410cca0746239dfaed006e841dc93f08f
asablock <005cga@163.com>
2024-09-01 19:29:19 +0800

    add restrictMaxLengthForSentChat

--- src/client/java/io/github/asablock/mdt/Toggles.java
+++ src/client/java/io/github/asablock/mdt/Toggles.java
@@ -6,9 +6,10 @@ import com.google.common.collect.HashBiMap;
 public final class Toggles {
     public static final BiMap<String, Toggle> TOGGLES = HashBiMap.create();
 
-    public static final Toggle disableChatMaxLength = of("disableChatMaxLength", true);
+    public static final Toggle disableChatFieldMaxLength = of("disableChatFieldMaxLength", true);
     public static final Toggle disableBlindness = of("disableBlindness", true);
     public static final Toggle disableDarkness = of("disableDarkness", true);
+    public static final Toggle restrictMaxLengthForSentChat = of("restrictMaxLengthForSentChat", true);
 
     private static Toggle of(String name, boolean defaultValue) {
         Toggle toggle = new Toggle(name, defaultValue);
--- src/client/java/io/github/asablock/mdt/mixin/MixinChatScreen.java
+++ src/client/java/io/github/asablock/mdt/mixin/MixinChatScreen.java
@@ -2,14 +2,22 @@ package io.github.asablock.mdt.mixin;
 
 import io.github.asablock.mdt.Toggles;
 import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ChatScreen.class)
 public class MixinChatScreen {
     @ModifyConstant(method = "init", constant = @Constant(intValue = 256))
     private int disableChatMaxLength(int value) {
-        return Toggles.disableChatMaxLength.enabled ? Integer.MAX_VALUE : value;
+        return Toggles.disableChatFieldMaxLength.enabled ? Integer.MAX_VALUE : value;
+    }
+
+    @Redirect(method = "sendMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendChatMessage(Ljava/lang/String;)V"))
+    private void sendChatMessageRestrict(ClientPlayNetworkHandler instance, String content) {
+        instance.sendChatMessage(Toggles.restrictMaxLengthForSentChat.enabled ? content.substring(0, 256) : content);
     }
 }
