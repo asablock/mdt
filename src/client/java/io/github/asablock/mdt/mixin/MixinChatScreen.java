db15672a8c2a8952408d05b67d44ed58839490c7
asablock <005cga@163.com>
2025-08-22 22:26:29 +0800

    fix(ui): 调整聊天框最大长度限制逻辑(防止ViaFabricPlus)

--- src/client/java/io/github/asablock/mdt/mixin/MixinChatScreen.java
+++ src/client/java/io/github/asablock/mdt/mixin/MixinChatScreen.java
@@ -1,19 +1,26 @@
 package io.github.asablock.mdt.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.asablock.mdt.toggle.Toggles;
 import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatScreen.class)
 public abstract class MixinChatScreen {
-    @ModifyExpressionValue(method = "init", at = @At(value = "CONSTANT", args = "intValue=256"))
-    private int disableChatMaxLength(int original) {
-        return Toggles.chatMaxLengthBehavior.get().shallRestrictFieldMaxLength() ? original : Integer.MAX_VALUE;
+    @Shadow protected TextFieldWidget chatField;
+
+    @Inject(method = "init", at = @At("TAIL"), order = 1100)
+    private void disableChatMaxLength(CallbackInfo ci) {
+        if (Toggles.chatMaxLengthBehavior.get().shallRestrictFieldMaxLength()) {
+            chatField.setMaxLength(Integer.MAX_VALUE);
+        }
     }
 
     @WrapOperation(method = "sendMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendChatMessage(Ljava/lang/String;)V"))
